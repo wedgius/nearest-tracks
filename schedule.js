42d04f47a177911dc7d723110af9e6795b0c97ee
Wyatt Gallagher <wedgius@users.noreply.github.com>
2017-09-11 13:34:40 -0700

    Add files via upload
    first real commit

---
+++ schedule.js
@@ -0,0 +1,236 @@
+var schedule = {
+	2016: [
+		{
+			track: 20,
+			sponsorName: "Alabama 500",
+			datetime: "2016-10-23"
+		},
+		{
+			track: 13,
+			sponsorName: "Goody's Fast Relief 500",
+			datetime: "2016-10-30"
+		},
+		{
+			track: 21,
+			sponsorName: "AAA Texas 500",
+			datetime: "2016-11-06"
+		},
+		{
+			track: 16,
+			sponsorName: "Can-Am 500",
+			datetime: "2016-11-13"
+		},
+		{
+			track: 8,
+			sponsorName: "Ford EcoBoost 400",
+			datetime: "2016-11-20"
+		}
+	],
+	2017: [
+		{
+			track: 6,
+			sponsorName: "The Unlimited",
+			datetime: "2017-02-18"
+		},
+		{
+			track: 6,
+			sponsorName: "Can-Am Duel at Daytona 1",
+			datetime: "2017-02-23"
+		},
+		{
+			track: 6,
+			sponsorName: "Can-Am Duel at Daytona 2",
+			datetime: "2017-02-23"
+		},
+		{
+			track: 6,
+			sponsorName: "Daytona 500",
+			datetime: "2017-02-26"
+		},
+		{
+			track: 0,
+			sponsorName: "Folds of Honor QuikTrip 500",
+			datetime: "2017-03-05"
+		},
+		{
+			track: 12,
+			sponsorName: "Kobalt 400",
+			datetime: "2017-03-12"
+		},
+		{
+			track: 16,
+			sponsorName: "Good Sam 500",
+			datetime: "2017-03-19"
+		},
+		{
+			track: 1,
+			sponsorName: "Auto Club 400",
+			datetime: "2017-03-26"
+		},
+		{
+			track: 13,
+			sponsorName: "STP 500",
+			datetime: "2017-04-02"
+		},
+		{
+			track: 21,
+			sponsorName: "O'Reilly Auto Parts 500",
+			datetime: "2017-04-09"
+		},
+		{
+			track: 2,
+			sponsorName: "Food City 500",
+			datetime: "2017-04-23"
+		},
+		{
+			track: 18,
+			sponsorName: "Toyota Owners 400",
+			datetime: "2017-04-30"
+		},
+		{
+			track: 20,
+			sponsorName: "Geico 500",
+			datetime: "2017-05-07"
+		},
+		{
+			track: 10,
+			sponsorName: "GoBowling.com 400",
+			datetime: "2017-05-13"
+		},
+		{
+			track: 3,
+			sponsorName: "NASCAR All-Star Showdown",
+			datetime: "2017-05-19"
+		},
+		{
+			track: 3,
+			sponsorName: "NASCAR All-Star Race",
+			datetime: "2017-05-20"
+		},
+		{
+			track: 3,
+			sponsorName: "Coca-Cola 600",
+			datetime: "2017-05-28"
+		},
+		{
+			track: 7,
+			sponsorName: "Dover Spring Race",
+			datetime: "2017-06-04"
+		},
+		{
+			track: 17,
+			sponsorName: "Pocono 400",
+			datetime: "2017-06-11"
+		},
+		{
+			track: 14,
+			sponsorName: "Firekeepers Casino 400",
+			datetime: "2017-06-18"
+		},
+		{
+			track: 19,
+			sponsorName: "Sonoma 350",
+			datetime: "2017-06-25"
+		},
+		{
+			track: 6,
+			sponsorName: "Coke Zero 400",
+			datetime: "2017-07-01"
+		},
+		{
+			track: 11,
+			sponsorName: "Kentucky 400",
+			datetime: "2017-07-08"
+		},
+		{
+			track: 15,
+			sponsorName: "New Hampshire 301",
+			datetime: "2017-07-16"
+		},
+		{
+			track: 9,
+			sponsorName: "Brickyard 400",
+			datetime: "2017-07-23"
+		},
+		{
+			track: 17,
+			sponsorName: "Pennsylvania 400",
+			datetime: "2017-07-30"
+		},
+		{
+			track: 22,
+			sponsorName: "Watkins Glen 355",
+			datetime: "2017-08-06"
+		},
+		{
+			track: 14,
+			sponsorName: "Pure Michigan 400",
+			datetime: "2017-08-13"
+		},
+		{
+			track: 2,
+			sponsorName: "Bass Pro Shops Night Race",
+			datetime: "2017-08-19"
+		},
+		{
+			track: 5,
+			sponsorName: "Bojangles' Southern 500",
+			datetime: "2017-09-03"
+		},
+		{
+			track: 18,
+			sponsorName: "Federated Auto Parts 400",
+			datetime: "2017-09-09"
+		},
+		{
+			track: 4,
+			sponsorName: "Chicagoland 400",
+			datetime: "2017-09-17"
+		},
+		{
+			track: 15,
+			sponsorName: "New England 300",
+			datetime: "2017-09-24"
+		},
+		{
+			track: 7,
+			sponsorName: "Dover Fall Race",
+			datetime: "2017-10-01"
+		},
+		{
+			track: 3,
+			sponsorName: "Charlotte 500",
+			datetime: "2017-10-07"
+		},
+		{
+			track: 20,
+			sponsorName: "Alabama 500",
+			datetime: "2017-10-15"
+		},
+		{
+			track: 10,
+			sponsorName: "Hollywood Casino 400",
+			datetime: "2017-10-22"
+		},
+		{
+			track: 13,
+			sponsorName: "Martinsville Fall Race",
+			datetime: "2017-10-29"
+		},
+		{
+			track: 21,
+			sponsorName: "AAA Texas 500",
+			datetime: "2017-11-05"
+		},
+		{
+			track: 16,
+			sponsorName: "Can-Am 500",
+			datetime: "2017-11-12"
+		},
+		{
+			track: 8,
+			sponsorName: "Ford EcoBoost 400",
+			datetime: "2017-11-19"
+		},
+	]
+}
---
+++ tracks.js
@@ -0,0 +1,186 @@
+var tracks = [
+	{	
+		name: "Atlanta Motor Speedway",			// 0
+		location: "Hampton, GA",
+		lat: 33.383494,
+		lng: -84.317856,
+		config: "oval",
+		shortName: "atlanta"
+	},
+	{	
+		name: "Auto Club Speedway",				// 1
+		location: "Fontana, CA",
+		lat: 34.08858,
+		lng: -117.50000,
+		config: "oval",
+		shortName: "fontana"
+	},
+	{	
+		name: "Bristol Motor Speedway",			// 2
+		location: "Bristol, TN",
+		lat: 36.516172,
+		lng: -82.256945,
+		config: "oval",
+		shortName: "bristol"
+	},
+	{	
+		name: "Charlotte Motor Speedway",		// 3
+		location: "Concord, NC",
+		lat: 35.350869,
+		lng: -80.683589,
+		config: "oval",
+		shortName: "charlotte"
+	},
+	{	
+		name: "Chicagoland Speedway",			// 4
+		location: "Joliet, IL",
+		lat: 41.474833,
+		lng: -88.057097,
+		config: "oval",
+		shortName: "chicagoland"
+	},
+	{	
+		name: "Darlington Raceway",				// 5
+		location: "Darlington, SC",
+		lat: 34.29562,
+		lng: -79.90596,
+		config: "oval",
+		shortName: "darlington"
+	},
+	{	
+		name: "Daytona International Speedway",	// 6
+		location: "Daytona Beach, FL",
+		lat: 29.185556,
+		lng: -81.069444,
+		config: "oval",
+		shortName: "daytona"
+	},
+	{	
+		name: "Dover International Speedway",	// 7
+		location: "Dover, DE",
+		lat: 39.18960,
+		lng: -75.53031,
+		config: "oval",
+		shortName: "dover"
+	},
+	{	
+		name: "Homestead-Miami Speedway",		// 8
+		location: "Homestead, FL",
+		lat: 25.45114,
+		lng: -80.40859,
+		config: "oval",
+		shortName: "homestead"
+	},
+	{	
+		name: "Indianapolis Motor Speedway",		// 9
+		location: "Speedway, IN",
+		lat: 39.798333,
+		lng: -86.232778,
+		config: "oval",
+		shortName: "indianapolis"
+	},
+	{	
+		name: "Kansas Speedway",					// 10
+		location: "Kansas City, KS",
+		lat: 39.115789,
+		lng: -94.831061,
+		config: "oval",
+		shortName: "kansas"
+	},
+	{	
+		name: "Kentucky Speedway",				// 11
+		location: "Sparta, KY",
+		lat: 38.70964,
+		lng: -84.91624,
+		config: "oval",
+		shortName: "kentucky"
+	},
+	{	
+		name: "Las Vegas Motor Speedway",		// 12
+		location: "Las Vegas, NV",
+		lat: 36.27134,
+		lng: -115.01112,
+		config: "oval",
+		shortName: "lasvegas"
+	},
+	{	
+		name: "Martinsville Speedway",			// 13
+		location: "Ridgeway, VA",
+		lat: 36.633889,
+		lng: -79.851111,
+		config: "oval",
+		shortName: "martinsville"
+	},
+	{	
+		name: "Michigan International Speedway",		// 14
+		location: "Brooklyn, MI",
+		lat: 42.0663,
+		lng: -84.241439,
+		config: "oval",
+		shortName: "michigan"
+	},
+	{	
+		name: "New Hampshire Motor Speedway",	// 15
+		location: "Loudon, NH",
+		lat: 43.36226,
+		lng: -71.46125,
+		config: "oval",
+		shortName: "loudon"
+	},
+	{	
+		name: "Phoenix International Raceway",	// 16
+		location: "Avondale, AZ",
+		lat: 33.37475,
+		lng: -112.31115,
+		config: "oval",
+		shortName: "phoenix"
+	},
+	{	
+		name: "Pocono Raceway",					// 17
+		location: "Long Pond, PA",
+		lat: 41.05539,
+		lng: -75.51152,
+		config: "oval",
+		shortName: "pocono"
+	},
+	{	
+		name: "Richmond International Raceway",	// 18
+		location: "Richmond, VA",
+		lat: 37.59169,
+		lng: -77.42091,
+		config: "oval",
+		shortName: "richmond"
+	},
+	{	
+		name: "Sonoma Raceway",					// 19
+		location: "Sonoma, CA",
+		lat: 38.16006,
+		lng: -122.45940,
+		config: "road",
+		shortName: "sonoma"
+	},
+	{	
+		name: "Talladega Superspeedway",			// 20
+		location: "Lincoln, AL",
+		lat: 33.566961,
+		lng: -86.066069,
+		config: "oval",
+		shortName: "talladega"
+	},
+	{	
+		name: "Texas Motor Speedway",			// 21
+		location: "Fort Worth, TX",
+		lat: 33.03689,
+		lng: -97.28309,
+		config: "oval",
+		shortName: "texas"
+	},
+	{	
+		name: "Watkins Glen International",		// 22
+		location: "Watkins Glen, NY",
+		lat: 42.336944,
+		lng: -76.927222,
+		config: "road",
+		shortName: "watkinsglen"
+	},
+];
